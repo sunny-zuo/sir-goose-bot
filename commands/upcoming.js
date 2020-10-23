9c283ae005363f35a40701d0aadadab9b546623c
Sunny Zuo <me@sunnyzuo.com>
2020-10-23 13:14:38 -0600

    feat: add relative time display to upcoming

--- commands/upcoming.js
+++ commands/upcoming.js
@@ -34,17 +34,17 @@ module.exports = {
         const outputEmbed = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Upcoming Dates for SE 25')
-            .setDescription('These are all upcoming quizzes, due dates, and other important dates for the upcoming week. Please contact Sunny (Dark#4321) if there are any issues');
-
+            .setDescription(`These are all upcoming quizzes, due dates, and other important dates for the upcoming week. Please contact <@${process.env.ADMIN_ID}> if there are any issues`)
+            .setFooter('https://github.com/sunny-zuo/sir-goose-bot');
         const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
         for (currentEvent of events) {
             const dt = DateTime.fromMillis(currentEvent.time, { zone: 'America/Toronto' });
             // if there is no specific time, trim it
             let dateFormat;
             if ((currentEvent.time - 1000 * 60 * 60 * 6) % (1000 * 60 * 60 * 24) == 0) {
-                dateFormat = dt.toLocaleString({ month: 'long', day: 'numeric', weekday: 'long' });
+                dateFormat = `${dt.toLocaleString({ month: 'long', day: 'numeric', weekday: 'long' })} (${dt.toRelative()})`;
             } else {
-                dateFormat = dt.toLocaleString({ month: 'long', day: 'numeric', hour: 'numeric', minute: '2-digit', timeZoneName: 'short', weekday: 'long' });
+                dateFormat = `${dt.toLocaleString({ month: 'long', day: 'numeric', hour: 'numeric', minute: '2-digit', timeZoneName: 'short', weekday: 'long' })} (${dt.toRelative()})`;
             }
 
             outputEmbed.addField(`${currentEvent.type}: ${currentEvent.name} for ${currentEvent.class}`, dateFormat)
