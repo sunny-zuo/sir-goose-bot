ad228f1f0c5d416c8c943919161e4f59c0cfeb3c
Sunny Zuo <me@sunnyzuo.com>
2021-01-12 19:32:06 -0700

    feat: migrate upcoming to db

--- commands/upcoming.js
+++ commands/upcoming.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 const { DateTime } = require("luxon");
+const mongo = require('../mongo.js');
 
 module.exports = {
     name: 'upcoming',
@@ -9,39 +10,26 @@ module.exports = {
     guildOnly: false,
     displayHelp: true,
     async execute(message, args) {
-        const data = JSON.parse(fs.readFileSync('./data/se25.json', 'utf-8'));
-        const fromDate = Date.now();
-        const toDate = fromDate + 1000 * 60 * 60 * 24 * 7 // ms => s => hr = > day => week
-        
-        const events = [];
-        
-        for (let i = 0; i < data.length; i++) {
-            if (data[i].time >= fromDate && data[i].time <= toDate) {
-                events.push(data[i]);
-            }
+        if (args && isNaN(parseFloat(args))) {
+            message.channel.send(new Discord.MessageEmbed().setColor('#FF0000').setTitle('Error').setDescription('You did not provide a valid number of days to display.').setFooter('https://github.com/sunny-zuo/sir-goose-bot'));
         }
-
-        events.sort((a, b) => {
-            if (a.time > b.time) {
-                return 1;
-            }
-            if (b.time > a.time) {
-                return -1;
-            }
-            return 0;
-        })
+        const fromDate = Date.now();
+        const toDate = fromDate + 1000 * 60 * 60 * 24 * ((args) ? parseFloat(args) : 7) // ms => s => hr = > day => week
+        
+        const events = await mongo.getDB().collection("tasks").find({ endTime: { $gte: new Date(fromDate), $lte: new Date(toDate) }}).sort({ endTime: 1 }).toArray();
 
         const outputEmbed = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Upcoming Dates for SE 25')
             .setDescription(`These are all upcoming quizzes, due dates, and other important dates for the upcoming week. Please contact <@${process.env.ADMIN_ID}> if there are any issues`)
             .setFooter('https://github.com/sunny-zuo/sir-goose-bot');
-        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+
         for (currentEvent of events) {
-            const dt = DateTime.fromMillis(currentEvent.time, { zone: 'America/Toronto' });
-            // if there is no specific time, trim it
+            const dt = DateTime.fromJSDate(currentEvent.endTime, { zone: 'America/Toronto' });
+
             let dateFormat;
-            if ((currentEvent.time - 1000 * 60 * 60 * 6) % (1000 * 60 * 60 * 24) == 0) {
+            if (currentEvent.ignoreTime) {
                 dateFormat = `${dt.toLocaleString({ month: 'long', day: 'numeric', weekday: 'long' })} (${dt.toRelative()})`;
             } else {
                 dateFormat = `${dt.toLocaleString({ month: 'long', day: 'numeric', hour: 'numeric', minute: '2-digit', timeZoneName: 'short', weekday: 'long' })} (${dt.toRelative()})`;
