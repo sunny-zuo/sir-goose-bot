860295c4310cf2df2d76d1accae654829ac2208e
ZhouJas <jasonzhou460@gmail.com>
2021-03-03 16:54:03 -0500

    Update pin.js

--- commands/pin.js
+++ commands/pin.js
@@ -6,18 +6,22 @@ module.exports = {
     description: 'pins the referenced message used when invoking this command',
     args: false,
     guildOnly: true,
-    displayHelp: true,
+    displayHelp: false,
     permissions: ['MANAGE_MESSAGES'],
     execute(message) {
+        var chnl = messages.channel;
         if (message.reference) {
-            var chnl = messages.channel;
             var referencedMessageID = message.reference.messageID;
             var referencedMessage;
             await chnl.messages.fetch(referencedMessageID).then(mmm => referencedMessage = mmm).catch(console.error);
-            await referencedMessage.pin({ reason: 'goose bot pin command invoked' }).then(messages => {
-                Log.success(`Pin request success for ${referencedMessage.content}`);
+            await referencedMessage.pin({ reason: 'goose bot pin command invoked' }).then(result => {
+                Log.success(`Pin request success for Message: ${referencedMessage.content}`);
             }).catch(console.error)
             return;
+        } else {
+            chnl.send("No referenced message for pinning! Use the reply function to add a referenced message and try again.").then(sentMessage => {
+                setTimeout(function () { sentMessage.delete(); }, 5000)
+            })
         }
     }
 }
