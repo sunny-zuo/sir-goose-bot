596e06e21752934648e5def4b7260a1f83f1e233
ZhouJas <jasonzhou460@gmail.com>
2021-03-03 16:54:56 -0500

    Functional pin-by-reply

--- commands/pin.js
+++ commands/pin.js
@@ -8,18 +8,19 @@ module.exports = {
     guildOnly: true,
     displayHelp: false,
     permissions: ['MANAGE_MESSAGES'],
-    execute(message) {
-        var chnl = messages.channel;
+    async execute(message) {
+        var chnl = message.channel;
         if (message.reference) {
             var referencedMessageID = message.reference.messageID;
+            console.log(referencedMessageID);
             var referencedMessage;
-            await chnl.messages.fetch(referencedMessageID).then(mmm => referencedMessage = mmm).catch(console.error);
-            await referencedMessage.pin({ reason: 'goose bot pin command invoked' }).then(result => {
+             await chnl.messages.fetch(referencedMessageID).then(foundMessage => referencedMessage=foundMessage).catch(console.error);
+             await referencedMessage.pin({ reason: 'goose bot pin command invoked' }).then(result => {
                 Log.success(`Pin request success for Message: ${referencedMessage.content}`);
             }).catch(console.error)
             return;
         } else {
-            chnl.send("No referenced message for pinning! Use the reply function to add a referenced message and try again.").then(sentMessage => {
+            chnl.send("No referenced message for pinning! Use the reply feature to add a referenced message and try again.").then(sentMessage => {
                 setTimeout(function () { sentMessage.delete(); }, 5000)
             })
         }
