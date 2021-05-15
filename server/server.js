d2c197e446f11f4057d52f43d11e8aa47f64f73a
Sunny Zuo <me@sunnyzuo.com>
2021-05-14 21:25:53 -0600

    feat: add option to automatically rename users

--- server/server.js
+++ server/server.js
@@ -57,7 +57,6 @@ async function assignRole(guild, guildSettings, user, userInfo) {
 
     if (!baseYear || !verificationRules) {
         throw new Error(`Server ${guild.name}/${guild.id} has invalid verification rules set.`);
-        return;
     }
     const roles = [];
 
@@ -75,6 +74,16 @@ async function assignRole(guild, guildSettings, user, userInfo) {
             }
             break;
         }
+    }
+    if (guild.me.hasPermission('MANAGE_NICKNAMES')) {
+        if (guildSettings.verificationRules?.renameFullName && userInfo.givenName && userInfo.surname) {
+            user.setNickname(`${userInfo.givenName} ${userInfo.surname}`);
+        } else if (guildSettings.verificationRules?.renameFirstName && userInfo.givenName) {
+            user.setNickname(userInfo.givenName);
+        }
+    }
+    else {
+        console.log(`Server ${guild.name}/${guild.id} has renaming enabled but has not granted the bot MANAGE_NICKNAMES permissions`);
     }
 
     return user.roles.add(roles, "Verified UW ID through bot");
