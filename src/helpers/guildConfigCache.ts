bc2f52b93e35afc138c0a0c5966a3212cc8dcb07
Sunny Zuo <me@sunnyzuo.com>
2021-07-15 22:21:46 -0600

    refactor: encapsulate GuildConfigCache

--- src/helpers/guildConfigCache.ts
+++ src/helpers/guildConfigCache.ts
@@ -1,22 +1,15 @@
 import { Collection, Snowflake } from 'discord.js';
 import GuildConfigModel, { GuildConfig } from '../models/guildConfig.model';
 
-const cache = new Collection<Snowflake, GuildConfig>();
-
-function getDefaultConfig(guildId: Snowflake): GuildConfig {
-    return {
-        guildId: guildId,
-        prefix: '$',
-        enableVerification: false,
-        enablePins: false,
-    };
-}
-
 export class GuildConfigCache {
+    private static _cache = new Collection<Snowflake, GuildConfig>();
+
     static async fetchConfig(guildId: Snowflake | undefined, bypassCache = false): Promise<GuildConfig> {
+        const cache = this._cache;
+
         if (guildId === undefined) {
             // TODO: make this less hacky/more clear
-            return getDefaultConfig(`${-1}`);
+            return this.getDefaultConfig(`${-1}`);
         }
 
         const cachedConfig: GuildConfig | undefined = cache.get(guildId);
@@ -25,18 +18,28 @@ export class GuildConfigCache {
             const guildConfig: GuildConfig | null = await GuildConfigModel.findOne({ guildId: guildId });
 
             if (guildConfig === null) {
-                return getDefaultConfig(guildId);
+                return this.getDefaultConfig(guildId);
             }
 
             cache.set(guildId, guildConfig);
             return guildConfig;
         } else {
+            console.log('hit cache');
             return cachedConfig;
         }
     }
 
     static updateCache(guildConfig: GuildConfig): void {
         const guildId = guildConfig.guildId;
-        cache.set(guildId, guildConfig);
+        this._cache.set(guildId, guildConfig);
+    }
+
+    private static getDefaultConfig(guildId: Snowflake): GuildConfig {
+        return {
+            guildId: guildId,
+            prefix: '$',
+            enableVerification: false,
+            enablePins: false,
+        };
     }
 }
