5035ec3b77f9ab57980df8900a14abdd747405e6
David Li <david.li3100@gmail.com>
2021-07-25 14:47:53 -0400

    feat: added command to manually ban all alternate accounts
    - finds the discord ID's UWID
    - finds all discord IDs associated with that UWID
    - asynchronously bans all accounts if they are in the server

---
+++ commands/banAlts.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const mongo = require('../mongo.js');
+// const { assignRole } = require('../server/server');
+
+const settings = require('../settings.js');
+
+module.exports = {
+    name: 'banalts',
+    description: 'Bans all alts of a verified Discord user using their Waterloo ID.',
+    args: true,
+    guildOnly: true,
+    displayHelp: true,
+    permissions: ['MANAGE_GUILD'],
+    async execute(message, args) {
+        const userID = args
+        const guildSettings = settings.get(message.guild?.id);
+        if (!guildSettings.verificationEnabled) {
+            return message.reply('This server does not have verification enabled');
+        }
+
+        message.channel.send(`Attempting to ban all alternate accounts of the user ${userID}`);
+        await message.guild.members.fetch();
+
+        // get specified user's UWID
+        const { uwid: userUWID } = await mongo.getDB().collection("users").findOne({ discordId: userID.id })
+        const altCounter = 0
+        // find all other discord accounts that share this UWID
+        for await (const user of mongo.getDB().collection("users").find({ uwid: userUWID })) {
+          // probably not very efficient for large servers, could just try/catch
+          // errors if user doesn't exist instead
+          if (message.guild.members.cache.values().includes(user.discordId)) {
+            // ban user
+            await message.guild.ban(user.discordId)
+            altCounter++
+          }
+        }
+
+        message.channel.send(`The user ${userID} had ${altCounter} alts that were successfully banned.`);
+    }
+}
