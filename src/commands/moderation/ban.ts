c849a0e91b3cc81880d32e3c75c7f8c35e050833
Sunny Zuo <me@sunnyzuo.com>
2021-08-13 13:30:25 -0600

    feat: ban by user id

--- src/commands/moderation/ban.ts
+++ src/commands/moderation/ban.ts
@@ -13,105 +13,169 @@ import BanModel from '../../models/ban.model';
 import UserModel from '../../models/user.model';
 import { Modlog } from '../../helpers/modlog';
 import { chunk } from '../../helpers/array';
+import { GuildConfigCache } from '../../helpers/guildConfigCache';
 
 export class Ban extends Command {
     private static readonly options: ApplicationCommandOption[] = [
         {
             name: 'user',
-            description: 'The user to ban.',
-            type: 'USER',
-            required: true,
+            description: 'Ban a user by mentioning them',
+            type: 'SUB_COMMAND',
+            options: [
+                {
+                    name: 'user',
+                    description: 'The user to ban.',
+                    type: 'USER',
+                    required: true,
+                },
+                {
+                    name: 'reason',
+                    description: 'The reason for the ban.',
+                    type: 'STRING',
+                    required: false,
+                },
+            ],
         },
         {
-            name: 'reason',
-            description: 'The reason for the ban.',
-            type: 'STRING',
-            required: false,
+            name: 'id',
+            description: 'Ban a user by their discord user id',
+            type: 'SUB_COMMAND',
+            options: [
+                {
+                    name: 'user_id',
+                    description: 'The discord user id of the user to ban.',
+                    type: 'STRING',
+                    required: true,
+                },
+                {
+                    name: 'reason',
+                    description: 'The reason for the ban.',
+                    type: 'STRING',
+                    required: false,
+                },
+            ],
         },
     ];
 
     constructor(client: Client) {
         super(client, {
             name: 'ban',
-            description: 'Ban a user and all known alt accounts using their Waterloo ID',
+            description: 'Ban a user and all known alt accounts linked via their UWaterloo ID',
             category: 'Moderation',
             options: Ban.options,
             guildOnly: true,
-            examples: ['@user', '@user reason'],
+            examples: ['id 123456789012345678 reason', 'user @user reason'],
             clientPermissions: [Permissions.FLAGS.BAN_MEMBERS],
             userPermissions: [Permissions.FLAGS.BAN_MEMBERS],
             cooldownSeconds: 2,
         });
     }
 
-    async execute(interaction: Message | CommandInteraction, args: CommandInteractionOptionResolver): Promise<void> {
-        const memberToBan = args.getMember('user') as GuildMember;
-        const banUserInfo = await UserModel.findOne({ discordId: memberToBan.id });
-        const banReason = args.getString('reason') ?? 'No reason provided.';
+    async execute(interaction: Message | CommandInteraction, args?: CommandInteractionOptionResolver): Promise<void> {
+        const guildConfig = await GuildConfigCache.fetchConfig(interaction.guild!.id);
+        // TODO: Refactor once help message supports subcommands
+        if (args === undefined || !args?.data || args.data.length === 0) {
+            return this.sendErrorEmbed(
+                interaction,
+                'Usage:',
+                `
+                Ban by Discord ID: \`${guildConfig.prefix}ban id 123456789012345678 reason\`
+                Ban by Discord user: \`${guildConfig.prefix}ban user @user reason\`
+                `
+            );
+        }
+
         const guild = interaction.guild;
         if (!guild) return;
+        await guild.members.fetch();
 
         const modlogEmbeds: MessageEmbed[] = [];
+        const banReason = args.getString('reason') ?? 'No reason provided.';
+        const providedUserId = args.getString('user_id') ?? (args.getMember('user') as GuildMember)?.id;
+        const memberToBan = guild.members.cache.get(providedUserId);
+
+        const banUserInfo = await UserModel.findOne({ discordId: providedUserId });
+        const possibleAlts =
+            banUserInfo && banUserInfo.uwid
+                ? await UserModel.find({ uwid: banUserInfo.uwid, discordId: { $ne: banUserInfo.discordId } })
+                : [];
+
+        if (memberToBan && !memberToBan.bannable) {
+            return this.sendErrorEmbed(interaction, 'Unable To Ban', `I do not have permission to ban ${memberToBan}.`);
+        }
+
+        for (const alt of possibleAlts) {
+            const altMember = guild.members.cache.get(alt.discordId);
+            if (altMember && !altMember.bannable) {
+                return this.sendErrorEmbed(
+                    interaction,
+                    'Unable To Ban',
+                    `I do not have permission to ban ${altMember}. (alt of the member you wanted to ban)`
+                );
+            }
+        }
 
         if (banUserInfo && banUserInfo.uwid) {
             const ban = new BanModel({
                 guildId: guild.id,
                 userId: banUserInfo.discordId,
                 uwid: banUserInfo.uwid,
-                reason: args.get('reason'),
+                reason: banReason,
                 bannedBy: (interaction.member as GuildMember).id,
             });
 
             await ban.save();
+        }
 
-            await guild.members.fetch();
+        for (const alt of possibleAlts) {
+            const altMember = guild.members.cache.get(alt.discordId);
+            if (altMember && altMember.bannable) {
+                const altBanMessage = `Banned by ${this.getUser(interaction).tag} | Alt of ${
+                    memberToBan?.user.tag ?? `user with id ${providedUserId}`
+                } | ${banReason}`;
+                await guild.bans.create(altMember, { reason: altBanMessage });
 
-            const possibleAlts = await UserModel.find({ uwid: banUserInfo.uwid, discordId: { $ne: banUserInfo.discordId } });
-
-            for (const alt of possibleAlts) {
-                const altMember = guild.members.cache.get(alt.discordId);
-                if (altMember) {
-                    const altBanMessage = `Banned by ${this.getUser(interaction).tag} | Alt of ${memberToBan.user.tag} | ${banReason}`;
-                    await guild.bans.create(altMember, { reason: altBanMessage });
-
-                    modlogEmbeds.push(
-                        Modlog.getUserEmbed(
-                            altMember.user,
-                            `   
+                modlogEmbeds.push(
+                    Modlog.getUserEmbed(
+                        altMember.user,
+                        `
                             **User**: ${altMember}
                             **Action**: Ban
-                            **Reason**: ${banReason} (alt of ${memberToBan.user.tag})
+                            **Reason**: ${banReason} (alt of ${memberToBan?.user.tag ?? `user with id ${providedUserId}`})
                             **Moderator**: ${interaction.member}
                             `,
-                            'RED'
-                        )
-                    );
-                }
+                        'RED'
+                    )
+                );
             }
         }
 
-        await memberToBan.send({
-            embeds: [
-                new MessageEmbed().setDescription(`You have been permanantly banned in ${guild.name} for: ${banReason}`).setColor('RED'),
-            ],
-        });
-        const banMessage = `Banned by ${this.getUser(interaction).tag} | ${banReason}`;
-        await guild.bans.create(memberToBan, { reason: banMessage });
+        if (memberToBan) {
+            await memberToBan
+                .send({
+                    embeds: [
+                        new MessageEmbed()
+                            .setDescription(`You have been permanantly banned in ${guild.name} for: ${banReason}`)
+                            .setColor('RED'),
+                    ],
+                })
+                .catch(() => this.client.log.error(`Failed to send ban message to ${memberToBan.user.tag}`));
 
-        modlogEmbeds.unshift(
-            Modlog.getUserEmbed(
-                memberToBan.user,
-                `   
+            modlogEmbeds.unshift(
+                Modlog.getUserEmbed(
+                    memberToBan.user,
+                    `
                     **User**: ${memberToBan}
                     **Action**: Ban
                     **Reason**: ${banReason}
                     **Moderator**: ${interaction.member}
                     `,
-                'RED'
-            )
-        );
+                    'RED'
+                )
+            );
 
-        if (modlogEmbeds.length > 1) {
+            await guild.bans.create(providedUserId, { reason: `Banned by ${this.getUser(interaction).tag} | ${banReason}` });
+
             await interaction.reply({
                 embeds: [
                     new MessageEmbed()
@@ -119,9 +183,21 @@ export class Ban extends Command {
                         .setColor('GREEN'),
                 ],
             });
+        } else if (banUserInfo && banUserInfo.uwid) {
+            await interaction.reply({
+                embeds: [
+                    new MessageEmbed()
+                        .setDescription(`**User ID ${providedUserId} and ${modlogEmbeds.length} alt accounts were banned |** ${banReason}`)
+                        .setColor('GREEN'),
+                ],
+            });
         } else {
             await interaction.reply({
-                embeds: [new MessageEmbed().setDescription(`**${memberToBan} was banned |** ${banReason}`).setColor('GREEN')],
+                embeds: [
+                    new MessageEmbed()
+                        .setDescription(`Unable to ban user ID ${providedUserId} - they are not verified and are not in this server.`)
+                        .setColor('YELLOW'),
+                ],
             });
         }
 
