8fe34842991aaa027a3786f1d9c45f64a23f7ebd
Sunny Zuo <me@sunnyzuo.com>
2021-09-10 11:59:52 -0400

    fix: silently fail when DMs are blocked

--- src/events/guildMemberAddEventHandler.ts
+++ src/events/guildMemberAddEventHandler.ts
@@ -20,29 +20,33 @@ export class GuildMemberAddEventHandler implements EventHandler {
             const assignedRoles = roleAssignmentResult.value;
 
             if (assignedRoles.length > 0) {
-                await member.send({
-                    embeds: [
-                        new MessageEmbed()
-                            .setTitle(`Welcome to ${member.guild.name}!`)
-                            .setColor('GREEN')
-                            .setDescription(
-                                `Since you've verified with the bot in the past, you've been automatically verified and have received the ${assignedRoles
-                                    .map((role) => `\`${role.name}\``)
-                                    .join(', ')} role(s).`
-                            ),
-                    ],
-                });
+                await member
+                    .send({
+                        embeds: [
+                            new MessageEmbed()
+                                .setTitle(`Welcome to ${member.guild.name}!`)
+                                .setColor('GREEN')
+                                .setDescription(
+                                    `Since you've verified with the bot in the past, you've been automatically verified and have received the ${assignedRoles
+                                        .map((role) => `\`${role.name}\``)
+                                        .join(', ')} role(s).`
+                                ),
+                        ],
+                    })
+                    .catch(); // we ignore errors since the user likely has DMs closed, so we just silently fail
             } else if ((await GuildConfigCache.fetchConfig(member.guild.id)).enableVerification) {
-                await member.send({
-                    embeds: [
-                        new MessageEmbed()
-                            .setTitle(`Welcome to ${member.guild.name}!`)
-                            .setColor('BLUE')
-                            .setDescription(
-                                "Since you've verified with the bot in the past, you've been automatically verified. However, the server has configured the bot to not assign any roles to you for various reasons (most likely due to only wanting to verify certain groups of people). If you think this is a mistake, please message a server admin."
-                            ),
-                    ],
-                });
+                await member
+                    .send({
+                        embeds: [
+                            new MessageEmbed()
+                                .setTitle(`Welcome to ${member.guild.name}!`)
+                                .setColor('BLUE')
+                                .setDescription(
+                                    "Since you've verified with the bot in the past, you've been automatically verified. However, the server has configured the bot to not assign any roles to you for various reasons (most likely due to only wanting to verify certain groups of people). If you think this is a mistake, please message a server admin."
+                                ),
+                        ],
+                    })
+                    .catch(); // we ignore errors since the user likely has DMs closed, so we just silently fail
             }
         }
     }
