5ff2a3ce5fb07ddd205bb3e074bbb0f0bf1524a1
Sunny Zuo <me@sunnyzuo.com>
2022-01-02 01:15:52 -0700

    feat: defer buttonrole reply (#25)

--- src/interactions/button/buttonRole.ts
+++ src/interactions/button/buttonRole.ts
@@ -23,14 +23,15 @@ export class ButtonRole implements ButtonInteractionHandler {
     async execute(interaction: ButtonInteraction, args?: string): Promise<void> {
         if (!args || !interaction.guild || !interaction.member) return;
 
+        await interaction.deferReply({ ephemeral: true }); // defer as role assginment can take more than 3 seconds during peak times
+
         let argData: InteractionData;
 
         try {
             argData = JSON.parse(args);
         } catch (e) {
-            await interaction.reply({
+            await interaction.editReply({
                 content: `This button role prompt is no longer valid.`,
-                ephemeral: true,
             });
             return this.client.log.warn(`Received invalid JSON from button role button interaction: ${args}`);
         }
@@ -40,9 +41,8 @@ export class ButtonRole implements ButtonInteractionHandler {
         const buttonRoleInfo = await ButtonRoleModel.findById(argData._id);
 
         if (!buttonRoleInfo || buttonRoleInfo.roles.every((roleData) => roleData.id !== argData.roleId)) {
-            await interaction.reply({
+            await interaction.editReply({
                 content: `This button role prompt is no longer valid.`,
-                ephemeral: true,
             });
             return this.client.log.warn(`Button role button interaction had invalid data: ${args}`);
         }
@@ -57,26 +57,24 @@ export class ButtonRole implements ButtonInteractionHandler {
                         .setDescription(`The role ${inlineCode(role.name)} was successfully removed.`)
                         .setColor('YELLOW');
                     await member.roles.remove(role);
-                    await interaction.reply({ embeds: [embed], ephemeral: true });
+                    await interaction.editReply({ embeds: [embed] });
                 } else {
                     const embed = new MessageEmbed()
                         .setDescription(`The role ${inlineCode(role.name)} was successfully added.`)
                         .setColor('GREEN');
                     await member.roles.add(role);
-                    await interaction.reply({ embeds: [embed], ephemeral: true });
+                    await interaction.editReply({ embeds: [embed] });
                 }
             } else {
-                await interaction.reply({
+                await interaction.editReply({
                     content: `I do not have permission to assign the role ${inlineCode(
                         role.name
                     )}. Please message a server admin to get this fixed!`,
-                    ephemeral: true,
                 });
             }
         } else {
-            await interaction.reply({
+            await interaction.editReply({
                 content: `The role you tried to assign does not exist. Server admins will need to recreate the button role prompt to fix this.`,
-                ephemeral: true,
             });
         }
     }
