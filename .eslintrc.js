98b13fbcbe31a55d5e2a8edb7cdd698ed1019a69
Sunny Zuo <me@sunnyzuo.com>
2022-01-16 19:45:01 -0700

    ci: check for usage of deprecated code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json',
+    },
+    plugins: ['@typescript-eslint', 'deprecation'],
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
     env: {
         node: true,
@@ -10,5 +15,6 @@ module.exports = {
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': ['error'],
         '@typescript-eslint/no-non-null-assertion': 'off',
+        'deprecation/deprecation': 'error',
     },
 };
