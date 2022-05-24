7012381ea71fddabc0644ea53f97d2687a0fa3e4
Sunny Zuo <me@sunnyzuo.com>
2022-05-24 18:56:07 -0400

    fix: make website image URIs valid (#51)

---
+++ src/web/public/_next/static/l1pv1a-svMo5MyrE4-d_Z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/156-fc9810904ae0ee1d.js","static/chunks/pages/index-da22c7d9b852f5b3.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
