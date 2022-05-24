d9ceea1d667a9588dbedc6881beeaf077d7903c3
Sunny Zuo <me@sunnyzuo.com>
2022-05-24 18:54:05 -0400

    fix: image url

--- src/web/public/_next/static/12Cv2nXwoX8Y3ztuRvlnN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/156-fc9810904ae0ee1d.js","static/chunks/pages/index-0e71459c7899e443.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
