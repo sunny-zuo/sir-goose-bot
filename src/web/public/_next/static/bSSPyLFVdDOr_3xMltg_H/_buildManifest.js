2217e82b4767a639e6b94cb0b4b3e957b23555a0
Sunny Zuo <me@sunnyzuo.com>
2022-09-08 23:36:48 -0700

    chore: update department list

--- src/web/public/_next/static/bSSPyLFVdDOr_3xMltg_H/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/156-fc9810904ae0ee1d.js","static/chunks/pages/index-b59f975319522f85.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
