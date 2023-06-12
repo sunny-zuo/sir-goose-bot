b8e248d4ba7c3061bc548a4d6b27d3e2b4737c2c
Sunny Zuo <me@sunnyzuo.com>
2023-06-11 19:07:21 -0700

    fix: course command query

--- src/util/uwflowConstants.ts
+++ src/util/uwflowConstants.ts
@@ -1,12 +1,13 @@
 import { gql } from 'graphql-request';
 
 export const uwflowQuery = gql`
-    query getCourse($code: String, $user_id: Int) {
+    query getCourse($code: String) {
         course(where: { code: { _eq: $code } }) {
-            ...CourseInfo
-            ...CourseRequirements
-            ...CourseRating
-            __typename
+        ...CourseInfo
+        ...CourseSchedule
+        ...CourseRequirements
+        ...CourseRating
+        __typename
         }
     }
     fragment CourseInfo on course {
@@ -15,18 +16,60 @@ export const uwflowQuery = gql`
         name
         description
         profs_teaching {
-            prof {
-                id
-                code
-                name
-                rating {
-                    liked
-                    comment_count
-                    __typename
-                }
-                __typename
+        prof {
+            id
+            code
+            name
+            rating {
+            liked
+            comment_count
+            __typename
             }
             __typename
+        }
+        __typename
+        }
+        __typename
+    }
+    fragment CourseSchedule on course {
+        id
+        sections {
+        id
+        enrollment_capacity
+        enrollment_total
+        class_number
+        section_name
+        term_id
+        updated_at
+        meetings {
+            days
+            start_date
+            end_date
+            start_seconds
+            end_seconds
+            location
+            prof {
+            id
+            code
+            name
+            __typename
+            }
+            is_closed
+            is_cancelled
+            is_tba
+            __typename
+        }
+        exams {
+            date
+            day
+            end_seconds
+            is_tba
+            location
+            section_id
+            start_seconds
+            __typename
+        }
+        __typename
         }
         __typename
     }
@@ -36,27 +79,27 @@ export const uwflowQuery = gql`
         prereqs
         coreqs
         postrequisites {
-            postrequisite {
-                id
-                code
-                name
-                __typename
-            }
+        postrequisite {
+            id
+            code
+            name
             __typename
+        }
+        __typename
         }
         __typename
     }
     fragment CourseRating on course {
         id
         rating {
-            liked
-            easy
-            useful
-            filled_count
-            comment_count
-            __typename
+        liked
+        easy
+        useful
+        filled_count
+        comment_count
+        __typename
         }
         __typename
-    }
+    }  
 `;
 export const uwflowEndpoint = 'https://uwflow.com/graphql';
