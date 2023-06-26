ad42587861239b34832dbe1696a85dca88c2d534
Sunny Zuo <me@sunnyzuo.com>
2023-06-25 22:05:55 -0700

    chore: update dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig');
 
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
