da0225406249d6a06de05bd3eb19a1c160ae76a1
celery <71736183+celery6@users.noreply.github.com>
2023-08-05 17:50:01 -0400

    verifyrules command can be text or slash command

--- src/commands/chat/verification/verifyrules.ts
+++ src/commands/chat/verification/verifyrules.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, MessageActionRow, MessageButton, MessageEmbed, Permissions } from 'discord.js';
+import { CommandInteraction, Message, MessageActionRow, MessageButton, MessageEmbed, Permissions } from 'discord.js';
 import { ChatCommand } from '../ChatCommand';
 import Client from '#src/Client';
 import { GuildConfigCache } from '../../../util/guildConfigCache';
@@ -14,12 +14,10 @@ export class VerifyRules extends ChatCommand {
             guildOnly: true,
             clientPermissions: [Permissions.FLAGS.MANAGE_ROLES],
             userPermissions: [Permissions.FLAGS.MANAGE_GUILD],
-            isSlashCommand: true,
-            isTextCommand: true,
         });
     }
 
-    async execute(interaction: CommandInteraction): Promise<void> {
+    async execute(interaction: Message | CommandInteraction): Promise<void> {
         const config = await GuildConfigCache.fetchConfig(interaction.guild!.id);
 
         const components: MessageActionRow[] = [];
@@ -28,7 +26,7 @@ export class VerifyRules extends ChatCommand {
         componentRow.addComponents(new MessageButton().setCustomId(`verifyRules`).setLabel('Update rules').setStyle('PRIMARY'));
         components.push(componentRow);
 
-        const embed = new MessageEmbed().setColor('GREEN').setTitle('Verification Rules').setDescription(`Verification is ${
+        const embed = new MessageEmbed().setColor('BLUE').setTitle('Verification Rules').setDescription(`Verification is ${
             config.enableVerification ? 'enabled ' : `disabled. Enable it using ${inlineCode('/config')}`
         }. [Create a ruleset.](https://sebot.sunnyzuo.com/)
                 ${codeBlock(serializeVerificationRules(config.verificationRules))}`);
