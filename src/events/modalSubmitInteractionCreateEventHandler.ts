dd6304c071c77cd29354448925971cd1a352d918
celery <71736183+celery6@users.noreply.github.com>
2023-08-27 15:49:33 -0400

    feat: update verifyrules command to use a button and modal
    Co-authored-by: Sunny Zuo <me@sunnyzuo.com>

---
+++ src/events/modalSubmitInteractionCreateEventHandler.ts
@@ -0,0 +1,45 @@
+import { Collection, Interaction } from 'discord.js';
+import { EventHandler } from './eventHandler';
+import Client from '#src/Client';
+import { logger } from '#util/logger';
+import ModalSubmitInteractionHandlers from '../interactions/modal';
+import { ModalSubmitInteractionHandler } from '../interactions/modal/modalInteractionHandler';
+
+export class ModalSubmitInteractionCreateEventHandler implements EventHandler {
+    readonly eventName = 'interactionCreate';
+    readonly client: Client;
+    readonly modalSubmitInteractionHandlers = new Collection<string, ModalSubmitInteractionHandler>();
+
+    constructor(client: Client) {
+        this.client = client;
+
+        for (const InteractionHandler of ModalSubmitInteractionHandlers) {
+            const interactionHandler = new InteractionHandler(client);
+            this.modalSubmitInteractionHandlers.set(interactionHandler.customId, interactionHandler);
+        }
+    }
+
+    async execute(interaction: Interaction): Promise<void> {
+        if (!interaction.isModalSubmit()) return;
+
+        logger.info(
+            {
+                modal: { customId: interaction.customId },
+                guild: { id: interaction.guild?.id ?? 'none' },
+                user: { id: interaction.user.id },
+            },
+            `Processing modal submission ${interaction.customId}`
+        );
+
+        const [interactionName] = interaction.customId.split('|');
+        const handler = this.modalSubmitInteractionHandlers.get(interactionName);
+
+        if (handler) {
+            try {
+                await handler.execute(interaction);
+            } catch (e) {
+                logger.error(e, e.message);
+            }
+        }
+    }
+}
---
+++ src/interactions/button/verifyRulesButton.ts
@@ -0,0 +1,31 @@
+import { ButtonInteraction, MessageActionRow, Modal, ModalActionRowComponent, TextInputComponent } from 'discord.js';
+import { ButtonInteractionHandler } from './buttonInteractionHandler';
+import { Cooldown } from '#util/cooldown';
+import Client from '#src/Client';
+
+export class VerifyRulesButton implements ButtonInteractionHandler {
+    readonly client: Client;
+    readonly customId = 'verifyRules';
+    readonly cooldown: Cooldown;
+
+    constructor(client: Client) {
+        this.client = client;
+        this.cooldown = new Cooldown(5, 3);
+    }
+
+    async execute(interaction: ButtonInteraction): Promise<void> {
+        const modal = new Modal().setCustomId('verifyRulesModal').setTitle('Verification Rules');
+
+        const ruleStringInput = new TextInputComponent()
+            .setCustomId('ruleStringInput')
+            .setLabel('Paste ruleset here:')
+            .setStyle('PARAGRAPH')
+            .setRequired(true);
+
+        const actionRow = new MessageActionRow<ModalActionRowComponent>().addComponents(ruleStringInput);
+
+        modal.addComponents(actionRow);
+
+        await interaction.showModal(modal);
+    }
+}
