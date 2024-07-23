69801f7627d5cd1288767607bf7ce71acd2bae9d
Sunny Zuo <me@sunnyzuo.com>
2024-07-22 22:07:52 -0700

    fix: defer course command (#85)

--- src/commands/chat/uwaterloo/course.ts
+++ src/commands/chat/uwaterloo/course.ts
@@ -2,7 +2,6 @@ import {
     ApplicationCommandOption,
     ChatInputCommandInteraction,
     EmbedBuilder,
-    Message,
     CommandInteractionOptionResolver,
     ApplicationCommandOptionType,
 } from 'discord.js';
@@ -10,6 +9,7 @@ import Client from '#src/Client';
 import { ChatCommand } from '../ChatCommand';
 import { request } from 'graphql-request';
 import { uwflowQuery, uwflowEndpoint } from '#util/uwflowConstants';
+import { logger } from '#util/logger';
 
 export class Course extends ChatCommand {
     private static readonly options: ApplicationCommandOption[] = [
@@ -35,86 +35,100 @@ export class Course extends ChatCommand {
     }
 
     async execute(
-        interaction: Message | ChatInputCommandInteraction,
+        interaction: ChatInputCommandInteraction,
         args: Omit<CommandInteractionOptionResolver, 'getMessage' | 'getFocused'>
     ): Promise<void> {
-        const courseMatcher = /([a-zA-Z]{2,}[ ]?\d+[a-zA-Z]?)/g;
-        const disallowedMatches = /(least)|(4U)/gi;
+        await interaction.deferReply();
 
-        const courseName = args.getString('course');
-        if (!courseName) return;
+        try {
+            const courseMatcher = /([a-zA-Z]{2,}[ ]?\d+[a-zA-Z]?)/g;
+            const disallowedMatches = /(least)|(4U)/gi;
 
-        const code = courseName.toLowerCase().replace(/[^a-z0-9]/g, '');
+            const courseName = args.getString('course');
+            if (!courseName) return;
 
-        const variables = {
-            code: code,
-        };
+            const code = courseName.toLowerCase().replace(/[^a-z0-9]/g, '');
 
-        const response = await request(uwflowEndpoint, uwflowQuery, variables);
+            const variables = {
+                code: code,
+            };
 
-        if (response.course.length < 1) {
+            const response = await request(uwflowEndpoint, uwflowQuery, variables);
+
+            if (response.course.length < 1) {
+                const embed = new EmbedBuilder()
+                    .setColor('Red')
+                    .setTitle('Error: No Course Found')
+                    .setDescription(`No course with the name '${courseName}' was found.`)
+                    .setTimestamp();
+
+                await interaction.reply({ embeds: [embed] });
+                return;
+            }
+
+            const replacer = (course: string) => {
+                const isDisallowed = disallowedMatches.test(course);
+                if (isDisallowed) {
+                    return course;
+                }
+                return `**[${course}](https://uwflow.com/course/${course.replace(/\s/g, '')})**`;
+            };
+
+            const trimString = (str: string, max: number) =>
+                str && str.length > max
+                    ? `${str
+                          .slice(0, max - 3)
+                          .split(' ')
+                          .slice(0, -1)
+                          .join(' ')}...`
+                    : str;
+
+            const course = response.course[0];
+            const prereqs = trimString(course.prereqs?.replace(courseMatcher, replacer), 1024) ?? 'None';
+            const antireqs = trimString(course.antireqs?.replace(courseMatcher, replacer), 1024) ?? 'None';
+
             const embed = new EmbedBuilder()
-                .setColor('Red')
-                .setTitle('Error: No Course Found')
-                .setDescription(`No course with the name '${courseName}' was found.`)
+                .setColor('Aqua')
+                .setTitle(`${course.code.toUpperCase()}: ${course.name}`)
+                .setDescription(`${course.description}\n**[View course on UWFlow](https://uwflow.com/course/${code})**`)
+                .addFields(
+                    {
+                        name: 'Prerequisites',
+                        value: prereqs,
+                    },
+                    {
+                        name: 'Antirequisites',
+                        value: antireqs,
+                    },
+                    {
+                        name: 'Liked',
+                        value: `${(course.rating.liked * 100).toFixed(2)}%`,
+                        inline: true,
+                    },
+                    {
+                        name: 'Easy',
+                        value: `${(course.rating.easy * 100).toFixed(2)}%`,
+                        inline: true,
+                    },
+                    {
+                        name: 'Useful',
+                        value: `${(course.rating.useful * 100).toFixed(2)}%`,
+                        inline: true,
+                    }
+                )
                 .setTimestamp();
 
-            await interaction.reply({ embeds: [embed] });
-            return;
+            await interaction.editReply({ embeds: [embed] });
+        } catch (e) {
+            logger.error(e, `course command error: ${e.message}`);
+
+            await interaction.editReply({
+                embeds: [
+                    new EmbedBuilder()
+                        .setColor('Red')
+                        .setDescription('An error occurred while fetching the course information. Please try again later.'),
+                ],
+            });
         }
-
-        const replacer = (course: string) => {
-            const isDisallowed = disallowedMatches.test(course);
-            if (isDisallowed) {
-                return course;
-            }
-            return `**[${course}](https://uwflow.com/course/${course.replace(/\s/g, '')})**`;
-        };
-
-        const trimString = (str: string, max: number) =>
-            str && str.length > max
-                ? `${str
-                      .slice(0, max - 3)
-                      .split(' ')
-                      .slice(0, -1)
-                      .join(' ')}...`
-                : str;
-
-        const course = response.course[0];
-        const prereqs = trimString(course.prereqs?.replace(courseMatcher, replacer), 1024) ?? 'None';
-        const antireqs = trimString(course.antireqs?.replace(courseMatcher, replacer), 1024) ?? 'None';
-
-        const embed = new EmbedBuilder()
-            .setColor('Aqua')
-            .setTitle(`${course.code.toUpperCase()}: ${course.name}`)
-            .setDescription(`${course.description}\n**[View course on UWFlow](https://uwflow.com/course/${code})**`)
-            .addFields(
-                {
-                    name: 'Prerequisites',
-                    value: prereqs,
-                },
-                {
-                    name: 'Antirequisites',
-                    value: antireqs,
-                },
-                {
-                    name: 'Liked',
-                    value: `${(course.rating.liked * 100).toFixed(2)}%`,
-                    inline: true,
-                },
-                {
-                    name: 'Easy',
-                    value: `${(course.rating.easy * 100).toFixed(2)}%`,
-                    inline: true,
-                },
-                {
-                    name: 'Useful',
-                    value: `${(course.rating.useful * 100).toFixed(2)}%`,
-                    inline: true,
-                }
-            )
-            .setTimestamp();
-
-        await interaction.reply({ embeds: [embed] });
     }
 }
